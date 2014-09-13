07486bd3420246149ceaf9e46c5a2fd88b028ee6
Karl Grogan <krlgrgn@gmail.com>
2014-09-13 12:02:11 +0100

    Initial commit with inherited UITableViewCell for swiping.

---
+++ SwipeCell/SwipeViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SwipeViewCell.h
+//  SwipeCell
+//
+//  Created by Karl Grogan on 13/09/2014.
+//  Copyright (c) 2014 Karl Grogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwipeViewCell : UITableViewCell
+
+@end
